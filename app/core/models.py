14cf8a2072f5aae4347d41228dd6d6887780f9ab
mattl <matt.lee.personal@gmail.com>
2022-07-12 17:46:07 -0500

    add superuser

--- app/core/models.py
+++ app/core/models.py
@@ -12,12 +12,21 @@ from django.contrib.auth.models import (
 class UserManager(BaseUserManager):
     """Manager for Users"""
     def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('User must have email address')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
         return user
 
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
 class User(AbstractBaseUser, PermissionsMixin):
     """User in the system"""
     email = models.EmailField(max_length=255, unique=True)

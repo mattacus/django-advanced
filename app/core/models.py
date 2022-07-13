e34e2b5a819631a5fd5d42212303b613f1719f2a
mattl <matt.lee.personal@gmail.com>
2022-07-12 22:52:28 -0500

    add recipe model

--- app/core/models.py
+++ app/core/models.py
@@ -39,3 +39,18 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     # Replace default user field used for auth
     USERNAME_FIELD = 'email'
+
+class Recipe(models.Model):
+    """Recipe Object"""
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.title
